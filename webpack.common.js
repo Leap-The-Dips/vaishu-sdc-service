5da44b61430121246c918a0424f62ae3b94d5174
Vaishnavi <vaishuthiyagaraj@gmail.com>
2019-11-20 13:25:57 -0800

    Configured gitignore and webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       },
       {
         test: /\.css$/,
